213932e29c48c14c24d87bf51d183e674bcf9e95
syahrul0505 <syahrulalif1234@gmail.com>
2022-12-10 13:01:27 +0700

    edit vendor

--- resources/views/backend/vendor/index.blade.php
+++ resources/views/backend/vendor/index.blade.php
@@ -99,10 +99,10 @@
                                             <i class="fas fa-eye"></i>
                                         </a>
 
-                                        <a href=""
+                                        {{-- <a href=""
                                             class="btn btn-secondary text-white">
                                             <i class="bx bxs-file-pdf"></i>
-                                        </a>
+                                        </a> --}}
 
                                         <div class="btn-group">
                                             @can('departement-edit')
