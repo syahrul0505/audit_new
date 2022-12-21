e08f5ec6117f7b50a6a6f6cc11348bfc84ff5748
syahrul0505 <syahrulalif1234@gmail.com>
2022-12-21 16:34:30 +0700

    garis

--- app/Http/Controllers/backend/VendorController.php
+++ app/Http/Controllers/backend/VendorController.php
@@ -169,13 +169,14 @@ class VendorController extends Controller
             $getCheckAccr = $this->checkAccr($request->no_po[$key],$request->tanggal_po[$key]);
             $replaceTitik = str_replace('.', '',$request->amount[$key]);
             $replaceComma = str_replace(',', '',$replaceTitik);
+            $replaceGaris = str_replace('_', '',$replaceComma);
             $vendorPivot[] = [
                 'vendor_id' => $vendor->id,
                 'no_po' => $request->no_po[$key],
                 'tanggal_po' => $request->tanggal_po[$key],
                 'no_invoice' => $request->no_invoice[$key],
                 'tanggal_kirim' => $request->tanggal_kirim[$key],
-                'amount' => $replaceComma,
+                'amount' => $replaceGaris,
                 'name' => $getCheckAccr[0]->VENDOR    
                 // 'amount' => $getCheckAccr[0]->AMOUNT  
             ];
